02b20e54b08e7e0aff84378b66035d1448fd372a
s221406603 <s221406603@M104-46>
2023-08-08 11:37:51 +0200

    modelB

---
+++ Qumbu_Community_Health_Care_Center/Models/Bookings.cs
@@ -0,0 +1,10 @@
+﻿namespace Qumbu_Community_Health_Care_Center.Models
+{
+    public class Bookings
+    {
+        public int BookingId { get; set; }
+        public string? Name { get; set; }
+        public DateTime Date { get; set; }
+        public TimeZone? Time { get; set; }
+    }
+}
