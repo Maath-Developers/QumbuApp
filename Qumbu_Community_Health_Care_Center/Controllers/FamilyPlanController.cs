ee4a4fcaf8cc3c3ffefe43d4b2da22cd10abc4ea
s223303682 <s223303682@M104-50>
2023-08-10 11:37:33 +0200

    Appointment

--- Qumbu_Community_Health_Care_Center/Controllers/FamilyPlanController.cs
+++ Qumbu_Community_Health_Care_Center/Controllers/FamilyPlanController.cs
@@ -21,6 +21,9 @@ namespace Qumbu_Community_Health_Care_Center.Controllers
         {
             return View();
         }
-
-    }
+		public IActionResult CreateAppointment()
+		{
+			return View();
+		}
+	}
 }
