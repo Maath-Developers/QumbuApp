001afb5169c3ae41a37ac2347042f2e8b15d2577
s221509100 <s221509100@R227-14>
2023-08-14 15:55:38 +0200

    counsellor

--- Qumbu_Community_Health_Care_Center/Areas/Identity/Data/ApplicationUser.cs
+++ Qumbu_Community_Health_Care_Center/Areas/Identity/Data/ApplicationUser.cs
@@ -27,7 +27,11 @@ public class ApplicationUser : IdentityUser
     public string? Role { get; set; }
     
     public int MobileNumber { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public ApplicationUser()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
         City = "Not set";
         Role = "Not Set";
