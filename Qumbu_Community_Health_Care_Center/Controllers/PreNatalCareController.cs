cd8d237ed645f159b5d17eb74e66fbd58fd81ef6
Asanda Monki <s219970661@mandela.ac.za>
2023-08-19 20:06:57 +0200

    New Updates

--- Qumbu_Community_Health_Care_Center/Controllers/PreNatalCareController.cs
+++ Qumbu_Community_Health_Care_Center/Controllers/PreNatalCareController.cs
@@ -2,15 +2,18 @@
 using Microsoft.EntityFrameworkCore;
 using Qumbu_Community_Health_Care_Center.Areas.Identity.Data;
 using Qumbu_Community_Health_Care_Center.Models;
+using System.Diagnostics;
 
 namespace Qumbu_Community_Health_Care_Center.Controllers
 {
     public class PreNatalCareController : Controller
     {
         private readonly ApplicationDbContext dbContext;
-        public PreNatalCareController(ApplicationDbContext _dbContext)
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public PreNatalCareController(ApplicationDbContext _dbContext, IWebHostEnvironment _webHostEnvironment)
         {
             dbContext = _dbContext;
+            webHostEnvironment = _webHostEnvironment;
         }
 
         public IActionResult Patient()
@@ -38,8 +41,19 @@ namespace Qumbu_Community_Health_Care_Center.Controllers
             IEnumerable<HealthRecord> objList = dbContext.HealthRecords;
             return View(objList);
         }
+        public IActionResult IndexAppointment()
+        {
+            IEnumerable<Appointment> objList = dbContext.Appointments;
+            return View(objList);
+        }
         [HttpGet]
         public IActionResult CreateRecord()
+        {
+            // Display a form for users to input health data
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateAppointment()
         {
             // Display a form for users to input health data
             return View();
@@ -55,6 +69,17 @@ namespace Qumbu_Community_Health_Care_Center.Controllers
                 return RedirectToAction("IndexRecord"); // Redirect to dashboard
             }
             return View(record);
+        }
+        public IActionResult CreateAppointment(Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the recorded health data to the database
+                dbContext.Appointments.Add(appointment);
+                dbContext.SaveChanges();
+                return RedirectToAction("IndexAppointment"); // Redirect to dashboard
+            }
+            return View(appointment);
         }
         public IActionResult UpdateRecord(int? ID)
         {
@@ -76,19 +101,70 @@ namespace Qumbu_Community_Health_Care_Center.Controllers
         {
             dbContext.HealthRecords.Update(record);
             dbContext.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("IndexRecord");
         }
-        public IActionResult Delete(int? ID)
+        public IActionResult UpdateAppointment(int? ID)
         {
-            var obj = dbContext.HealthRecords.Find(ID);
+            if (ID == null || ID == 0)
+            {
+                return NotFound();
+            }
+            var obj = dbContext.Appointments.Find(ID);
             if (obj == null)
             {
                 return NotFound();
             }
 
-            dbContext.HealthRecords.Remove(obj);
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateAppointment(Appointment appointment)
+        {
+            dbContext.Appointments.Update(appointment);
+            dbContext.SaveChanges();
+            return RedirectToAction("IndexAppointment");
+        }
+        public IActionResult Delete(int? ID)
+        {
+            var obj = dbContext.Appointments.Find(ID);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Appointments.Remove(obj);
             dbContext.SaveChanges();
             return RedirectToAction("Index");
         }
+        //public IActionResult IndexUltrasoundAppointment()
+        //{
+        //    IEnumerable<UltrasoundAppointment> objList = dbContext.Ultrasounds;
+        //    return View();
+        //}
+        //public IActionResult CreateUltrasoundAppointment(UltrasoundAppointment ultrasound)
+        //{
+        //    string uniqueFileName = null;
+        //    if (ultrasound.ImageFile != null)
+        //    {
+        //        string ImageUploadedFolder = Path.Combine
+        //            (webHostEnvironment.WebRootPath, "UploadedImages");
+        //        uniqueFileName = Guid.NewGuid().ToString() + " " + ultrasound.ImageFile.FileName;
+
+        //        string filepath = Path.Combine(ImageUploadedFolder, uniqueFileName);
+
+        //        using (var fileStream = new FileStream(filepath, FileMode.Create))
+        //        {
+        //            ultrasound.ImageFile.CopyTo(fileStream);
+        //        }
+        //        ultrasound.UltrasoundImagePath = "~/wwwroot/UploadedImages";
+        //        ultrasound.FileName = uniqueFileName;
+
+        //        dbContext.Ultrasounds.Add(ultrasound);
+        //        dbContext.SaveChanges();
+        //        return RedirectToAction("IndexUltrasoundAppointment");
+        //    }
+        //    return View();
+        //}
     }
 }
---
+++ Qumbu_Community_Health_Care_Center/Models/UltrasoundAppointment.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Qumbu_Community_Health_Care_Center.Models
+{
+    //public class UltrasoundAppointment
+    //{
+    //    [Key]
+    //    public int Id { get; set; }
+    //    [Required]
+    //    public string UserId { get; set; }
+    //    [Required]
+    //    public DateTime AppointmentDate { get; set; }
+    //    [Required]
+    //    public int GestationalAgeWeeks { get; set; }
+    //    [Required]
+    //    public string FetalHeartRate { get; set; }
+    //    public string Observations { get; set; }
+    //    public bool GenderDetermined { get; set; }
+    //    public string Comments { get; set; }
+    //    [Required]
+    //    public string UltrasoundImagePath { get; set; }
+    //    public string FileName { get; set; }
+    //    [DisplayName("Upload File")]
+    //    [NotMapped]
+    //    public IFormFile ImageFile { get; set; }
+
+    //}
+}
