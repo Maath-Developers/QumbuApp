1a9181fb244e2a06c2114ec75c6d10759fe1b195
Asanda Monki <s219970661@mandela.ac.za>
2023-09-13 18:40:28 +0200

    Email

--- Qumbu_Community_Health_Care_Center/Program.cs
+++ Qumbu_Community_Health_Care_Center/Program.cs
@@ -1,19 +1,22 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Qumbu_Community_Health_Care_Center.Areas.Identity.Data;
+using Qumbu_Community_Health_Care_Center.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Qumbu_Community_Health_Care_Center/Services/EmailSender.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using System.Net;
+using System.Net.Mail;
+
+namespace Qumbu_Community_Health_Care_Center.Services
+{
+    public class EmailSender: IEmailSender
+    {
+        public EmailSender() 
+        {
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            string from = "monkiMajor@gmail.com";
+            string Password = "sydroybluqeorckp";
+            string SenderName = "Admin";
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(from);
+            message.Subject = subject;
+            message.Body = "<html><body>" + htmlMessage + "</html></body>";
+            message.To.Add(new MailAddress(email));
+            message.IsBodyHtml = true;
+
+            var smtpclient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(from, Password),
+                EnableSsl = true,
+            };
+            smtpclient.Send(message);
+        }
+    }
+}
