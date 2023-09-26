81844cda62afafdb36cead3d0b0698d17c5582c5
s221406603 <s221406603>
2023-09-26 15:23:54 +0200

    counsellor

--- Qumbu_Community_Health_Care_Center/Models/Appointment.cs
+++ Qumbu_Community_Health_Care_Center/Models/Appointment.cs
@@ -1,17 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Qumbu_Community_Health_Care_Center.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Qumbu_Community_Health_Care_Center.Models
 {
 	public class Appointment
 	{
 		[Key]
-		public int AppointmentId { get; set; }
-
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public DateTime Date { get; set; }
-
+		public int AppointmentID { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		[DataType(DataType.DateTime)]
+		public DateTime Date { get; set; } = DateTime.Now;
+        [Required]
+        public string? Purpose { get; set; }
+		public string? Status { get; set; } = "New";
+        public string? PatientID { get; set; }
+		[ForeignKey("PatientID")]
+		public virtual ApplicationUser? MainUser { get; set; }
+		[Required]
+		[Display(Name = "Appointment Day")]
+		[DataType(DataType.DateTime)]
 		public TimeSpan? Time { get; set; }
+		public DateTime Date_Time { get; set; }	
 		//public string? Notes { get; set; }
 	}
 }
