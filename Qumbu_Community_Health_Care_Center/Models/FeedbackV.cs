f9edb850af6a1669228488ad10678804834ab187
Hlulelwa Dudula <s223060690@mandela.ac.za>
2023-09-28 09:35:06 +0200

    Feedback

--- Qumbu_Community_Health_Care_Center/Models/FeedbackV.cs
+++ Qumbu_Community_Health_Care_Center/Models/FeedbackV.cs
@@ -10,6 +10,31 @@ namespace Qumbu_Community_Health_Care_Center.Models
         [Key]
         public int ID { get; set; }
 
+        [Required]
+        [DisplayName("First Name")]
+        public string? Name { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        public string? Surname { get; set; }
+        [Required]
+        [DisplayName("Very Good")]
+        public string? rating1 { get; set; }
+        [Required]
+        [DisplayName("Good")]
+        public string? rating2 { get; set; }
+        [Required]
+        [DisplayName("Mediocre")]
+        public string? rating3 { get; set; }
+        [Required]
+        [DisplayName("Bad")]
+        public string? rating4 { get; set; }
+        [Required]
+        [DisplayName("Very Bad")]
+        public string? rating5 { get; set; }
+        [Required]
+        [DisplayName("Leave your comment here")]
+        public string? Comnent { get; set; }
+
 
     }
 }
