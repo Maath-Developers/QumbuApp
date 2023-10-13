b5956dc7cb6bff0442e7ba3a380b8f1e8096b8aa
s223303682@mandela.ac.za <s223303682@M104-49>
2023-10-13 13:41:41 +0200

    Family

--- Qumbu_Community_Health_Care_Center/Controllers/FamilyPlanController.cs
+++ Qumbu_Community_Health_Care_Center/Controllers/FamilyPlanController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Qumbu_Community_Health_Care_Center.Areas.Identity.Data;
@@ -10,8 +11,10 @@ namespace Qumbu_Community_Health_Care_Center.Controllers
     public class FamilyPlanController : Controller
     {
         private readonly ApplicationDbContext dbContext;
-        public FamilyPlanController(ApplicationDbContext dbC)
+        private readonly IEmailSender _email;
+        public FamilyPlanController(ApplicationDbContext dbC, IEmailSender email)
         {
+            _email = email;
             dbContext = dbC;
         }
 
@@ -150,6 +153,56 @@ namespace Qumbu_Community_Health_Care_Center.Controllers
 			ViewData["PatientID"] = new SelectList(dbContext.Users, "Id", "Id", familyReg.PatientID);
 			return View(familyReg);
 		}
+        public IActionResult AcceptReg(int? ID)
+        {
+            var familyReg = dbContext.FamilyReg.Find(ID);
+            if (familyReg != null)
+            {
+                familyReg.Status = "Accepted";
+                dbContext.FamilyReg.Update(familyReg);
+                dbContext.SaveChanges();
+                TempData["Success"] = "The appointment has been accepted";
+                var patient = dbContext.Users.Where(a => a.Id == familyReg.PatientID).FirstOrDefault();
+                try
+                {
+                    string supportEmail = "Qumbu_Community_Health_Care_Center.healthcare@gmail.com";
+                    _email.SendEmailAsync(patient.Email, "Confirm your email",
+                        $"<html><head><style>body{{font-family:Arial,sans-serif;}}" +
+                        $"h1{{color:#336699;}}" +
+                        $".cta-button{{background-color:#336699;color:@fff;" +
+                        $"padding:10px 20px;" +
+                        $"text-decoration:none;border-radius:5px;}}" +
+                        $".cta-button:hover{{background-color:#265580;}}" +
+                        $".footer{{margin-top:20px;font-size:12px;color:#888;}}" +
+                        $"</style>" +
+                        $"</head>" +
+                        $"<body>" +
+                        $"" +
+                        $"<h1>Qumbu Healthcare Center!</h1>" +
+                        $"<p></p>" +
+                        $"<p> Thank for booking with us.</P>" +
+                        $"<p>Take note that your appointment has been accepted</P>" +
+                        $"<Strong><p>Appointment Date: {familyReg.Date}</P></strong>" +
+                        $"<Strong><p>Appointment Purpose: {familyReg.Status}</P></strong>" +
+                        $"<p>Please be on time, atleast 10 minutes before</P>" +
+
+                        $"" +
+                        $"if you have any question ,contact our team at {supportEmail}</p>" +
+                        $"<dic class='footer'>" +
+                        $"<p>Thank you</p>" +
+                        $"</div>" +
+                        $"</body>" +
+                        $"</html>");
+                }
+                catch
+                {
+
+                }
+                return RedirectToAction("All_Appointments");
+            }
+            ViewData["PatientID"] = new SelectList(dbContext.Users, "Id", "Id", familyReg.PatientID);
+            return View(familyReg);
+        }
 
         public async Task <IActionResult> ViewFamilyReg()
         {
@@ -169,41 +222,41 @@ namespace Qumbu_Community_Health_Care_Center.Controllers
 			IEnumerable<Appointment> oblList = dbContext.Appointments;
 			return View(oblList);
 		}
-		public IActionResult updateAppointment(int? ID)
-		{
-			if (ID == null || ID == 0)
-			{
-				return NotFound();
+		//public IActionResult updateAppointment(int? ID)
+		//{
+		//	if (ID == null || ID == 0)
+		//	{
+		//		return NotFound();
 
-			}
-			var list = dbContext.Appointments.Find(ID);
-			if (list == null)
-			{
-				return NotFound();
-			}
-			return View(list);
-		}
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult updateAppointment(Appointment appointment)
-		{
-			dbContext.Appointments.Update(appointment);
-			dbContext.SaveChanges();
-			return RedirectToAction("ViewAppointment");
-		}
+		//	}
+		//	var list = dbContext.Appointments.Find(ID);
+		//	if (list == null)
+		//	{
+		//		return NotFound();
+		//	}
+		//	return View(list);
+		//}
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public IActionResult updateAppointment(Appointment appointment)
+		//{
+		//	dbContext.Appointments.Update(appointment);
+		//	dbContext.SaveChanges();
+		//	return RedirectToAction("ViewAppointment");
+		//}
 
-		public IActionResult DeleteAppointment(int? ID)
-		{
-			var list = dbContext.Appointments.Find(ID);
-			if (list == null)
-			{
-				return NotFound();
-			}
-			dbContext.Appointments.Remove(list);
-			dbContext.SaveChanges();
-			return RedirectToAction("ViewAppointment");
+		//public IActionResult DeleteAppointment(int? ID)
+		//{
+		//	var list = dbContext.Appointments.Find(ID);
+		//	if (list == null)
+		//	{
+		//		return NotFound();
+		//	}
+		//	dbContext.Appointments.Remove(list);
+		//	dbContext.SaveChanges();
+		//	return RedirectToAction("ViewAppointment");
 
-		}
+		//}
         public ActionResult ViewReports()
         {
             return View();
