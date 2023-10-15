de341abbf23823a082385db4384478c310154184
Thabo Frank Mazibuko <s221509100@mandela.ac.za>
2023-10-15 01:55:13 -0800

    fix merges

--- Qumbu_Community_Health_Care_Center/Controllers/PatientRegController.cs
+++ Qumbu_Community_Health_Care_Center/Controllers/PatientRegController.cs
@@ -17,14 +17,11 @@ namespace Qumbu_Community_Health_Care_Center.Controllers
             IEnumerable<PatientReg> list = dbContext.PatientReg;
             return View(list);
         }
-<<<<<<< HEAD
         public IActionResult patientList()
         {
             IEnumerable<PatientReg> list = dbContext.PatientReg;
             return View(list);
         }
-=======
->>>>>>> bbaa07f14c9e1dfd5a2a9b23006839eed12aa88a
         public IActionResult Create_File()
         {
             return View();
@@ -32,21 +29,14 @@ namespace Qumbu_Community_Health_Care_Center.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-<<<<<<< HEAD
-=======
 
->>>>>>> bbaa07f14c9e1dfd5a2a9b23006839eed12aa88a
         public IActionResult Create_File(PatientReg patientReg)
         {
             if (ModelState.IsValid)
             {
                 dbContext.PatientReg.Add(patientReg);
                 dbContext.SaveChanges();
-<<<<<<< HEAD
                 return RedirectToAction("fileList");
-=======
-                return RedirectToAction("patentList");
->>>>>>> bbaa07f14c9e1dfd5a2a9b23006839eed12aa88a
             }
             return View(patientReg);
         }
