ecf10a19b062bc9753393d0fbc28a8beba5b8a7a
Thabo Frank Mazibuko <s221509100@mandela.ac.za>
2023-10-15 01:41:46 -0800

    update

---
+++ Qumbu_Community_Health_Care_Center/Controllers/PatientRegController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Qumbu_Community_Health_Care_Center.Areas.Identity.Data;
+using Qumbu_Community_Health_Care_Center.Models;
+
+namespace Qumbu_Community_Health_Care_Center.Controllers
+{
+    public class PatientRegController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public PatientRegController(ApplicationDbContext dBD)
+        {
+            dbContext= dBD;
+        }
+        public IActionResult fileList()
+        {
+            IEnumerable<PatientReg> list = dbContext.PatientReg;
+            return View(list);
+        }
+        public IActionResult patientList()
+        {
+            IEnumerable<PatientReg> list = dbContext.PatientReg;
+            return View(list);
+        }
+        public IActionResult Create_File()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create_File(PatientReg patientReg)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.PatientReg.Add(patientReg);
+                dbContext.SaveChanges();
+                return RedirectToAction("fileList");
+            }
+            return View(patientReg);
+        }
+    }
+}
