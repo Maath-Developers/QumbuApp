4193294aa0d63e9bf87bcb4c9068e14b0d367a69
Hlulelwa Dudula <s223060690@mandela.ac.za>
2023-10-16 11:40:16 +0200

    Dashboard

---
+++ Qumbu_Community_Health_Care_Center/Migrations/20231016081035_AddDB.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Qumbu_Community_Health_Care_Center.Migrations
+{
+    public partial class AddDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AppointmentID",
+                table: "Profiling",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "kinCell",
+                table: "PatientReg",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppointmentID",
+                table: "Profiling");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "kinCell",
+                table: "PatientReg",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
