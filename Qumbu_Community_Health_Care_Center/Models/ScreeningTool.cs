2c9aaa6e755b20db1431d31f822bc0e854c44246
Hlulelwa Dudula <s223060690@mandela.ac.za>
2023-10-18 21:45:59 +0200

    Feedback

--- Qumbu_Community_Health_Care_Center/Models/ScreeningTool.cs
+++ Qumbu_Community_Health_Care_Center/Models/ScreeningTool.cs
@@ -19,36 +19,45 @@ namespace Qumbu_Community_Health_Care_Center.Models
 
 		[Required]
 		[DisplayName("1) Do you have any allergies ?")]
-		public string? Question1 { get; set; }
+		public int? Question1 { get; set; }
+
 		[Required]
 		[DisplayName("2) Are you pregnant? ?")]
-		public string? Question2 { get; set; }
+		public int? Question2 { get; set; }
+
 		[Required]
 		[DisplayName("3) Are you sick today ?")]
 		public int Question3 { get; set; }
+
 		[Required]
 		[DisplayName("4) Are you coughing")]
-		public string? Question4 { get; set; }
+		public int? Question4 { get; set; }
+
 		[Required]
 		[DisplayName("5) Do you have shortness of breath or difficulty in breathing ?")]
-		public string? Question5 { get; set; }
+		public int? Question5 { get; set; }
+
 		[Required]
 		[DisplayName("6) Do you have headache ?")]
-		public string? Question6 { get; set; }
-		//public List<string> Symptoms { get; set; }
+		public int? Question6 { get; set; }
 
-		//public bool IsHighRisk { get; set; }
+        [Required]
+        [DisplayName("Date")]
+        public DateTime? Date { get; set; } = DateTime.Now;
+        //public List<string> Symptoms { get; set; }
 
-		//public ScreeningTool()
-		//{
-		//    Symptoms = new List<string>();
-		//}
+        //public bool IsHighRisk { get; set; }
 
-		//public void CalculateRisk()
-		//{
-		//    IsHighRisk = (Age > 60) || (Symptoms.Contains("Fever") && Symptoms.Contains("Cough"));
-		//}
+        //public ScreeningTool()
+        //{
+        //    Symptoms = new List<string>();
+        //}
+
+        //public void CalculateRisk()
+        //{
+        //    IsHighRisk = (Age > 60) || (Symptoms.Contains("Fever") && Symptoms.Contains("Cough"));
+        //}
 
 
-	}
+    }
 }
